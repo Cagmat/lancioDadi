bdec230ea3f65512cbc494b58fcd88550da5e76f
Gab2006 <tostigabriele6@gmail.com>
2025-04-03 13:30:03 +0200

    03/04/25

--- app/src/main/java/com/example/cagliesi_lanciodadi/MainActivity.kt
+++ app/src/main/java/com/example/cagliesi_lanciodadi/MainActivity.kt
@@ -14,18 +14,42 @@ import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     @SuppressLint("MissingInflatedId")
+    private val TAG = "MainActivity"
+    private lateinit var btnRoll
+    private lateinit var txtTitle
+    private lateinit var txtNumber
+    private lateinit var imgDice
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        var btnRoll = findViewById<Button>(R.id.buttonLancia)
-        var txtTitle = findViewById<TextView>(R.id.TextViewTitle)
-        var imgDice = findViewById<ImageView>(R.id.imageViewDice)
+        Log.d(TAG,"Inflate fatto")
+        btnRoll = findViewById<Button>(R.id.buttonLancia)
+        txtTitle = findViewById<TextView>(R.id.TextViewTitle)
+        txtNumber = findViewById<TextView>(R.id.TextViewNumber)
+        imgDice = findViewById<ImageView>(R.id.imageViewDice)
 
         btnRoll.setOnClickListener(View.OnClickListener {
             val toast = Toast.makeText(this, "LANCIO IN CORSO", Toast.LENGTH_SHORT)
             toast.show()
         })
 
+    }
+
+    public function rollDice(){
+        Log.d(TAG,"Lancio del dado")
+        val numeroRandom = Random().nextInt(6)+1
+        Log.d(TAG,"Lancio del dado: " + numeroRandom.toString())
+        txtNumber.text = numeroRandom.toString()
+        val imgDiceResources  = when(numeroRandom){
+            1 -> R.drawable.dice1
+            2 -> R.drawable.dice2
+            3 -> R.drawable.dice3
+            4 -> R.drawable.dice4
+            5 -> R.drawable.dice5
+            else 6 -> R.drawable.dice6
+        }
+        imgDice.setImageResource(imgDiceResources)
     }
 }
